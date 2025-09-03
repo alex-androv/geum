08f1881e9e2d7332546ebbc0581121cb9b0aff1a
Alexey Alexandrov <alex.androv37@yahoo.com>
2025-09-03 15:45:23 +0500

    footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,53 @@
-<script setup></script>
+<script setup>
+import { useRoute } from 'vue-router'
+import { computed } from 'vue'
+import InstagramIcon from '@/assets/icons/instagram-bold.svg'
+import WhatsappIcon from '@/assets/icons/whatsapp-bold.svg'
+import WoltIcon from '@/assets/icons/wolt-bold.svg'
+import GoogleIcon from '@/assets/icons/google-bold.svg'
+
+const route = useRoute()
+
+const isNotHomePage = computed(() => route.name !== 'home')
+</script>
 
 <template>
   <div
-    class="bg-[#F0F0F0] px-2 xs:px-3 sm:px-5 lg:px-10 2xl:px-[224px] py-4 border-t border-[#919191]"
+    v-if="isNotHomePage"
+    class="bg-[#F0F0F0] border-t border-[#919191] grid grid-cols-2 gap-4 px-2 pt-4 pb-8"
+  >
+    <a
+      href="https://www.instagram.com/geum.bistro/"
+      target="_blank"
+      class="flex flex-col items-center gap-1"
+    >
+      <InstagramIcon />
+      <p class="font-fliege font-medium text-[20px] leading-[24px] text-[#2D2A28]">instagram</p>
+    </a>
+    <a href="https://wa.me/381611128101" target="_blank" class="flex flex-col items-center gap-1">
+      <WhatsappIcon />
+      <p class="font-fliege font-medium text-[20px] leading-[24px] text-[#2D2A28]">whatsapp</p>
+    </a>
+    <a
+      href="https://maps.app.goo.gl/uPy9nPYbt8Vdps8V6?g_st=com.google.maps.preview.copy"
+      target="_blank"
+      class="flex flex-col items-center gap-1 ml-8"
+    >
+      <GoogleIcon />
+      <p class="font-fliege font-medium text-[20px] leading-[24px] text-[#2D2A28]">google</p>
+    </a>
+    <a
+      href="https://wolt.com/sr/srb/belgrade/restaurant/geum-bistro"
+      target="_blank"
+      class="flex flex-col items-center gap-1 ml-8"
+    >
+      <WoltIcon />
+      <p class="font-fliege font-medium text-[20px] leading-[24px] text-[#2D2A28]">wolt</p>
+    </a>
+  </div>
+
+  <div
+    class="bg-[#F0F0F0] px-2 xs:px-3 sm:px-5 lg:px-10 2xl:px-[224px] py-4 border-t border-[#C4C4C4]"
   >
     <div class="flex flex-col sm:flex-row sm:justify-between gap-6 mb-6 items-center">
       <a
